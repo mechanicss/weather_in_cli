d596893b351115d40d30f6d6f618013d049a3dd3
mechanicss <50999677+mechanicss@users.noreply.github.com>
2023-11-10 00:46:48 +0300

    edits as assigned

--- weather_in_terminal.py
+++ weather_in_terminal.py
@@ -1,80 +1,45 @@
-"""
-Использование:
-
-    $ curl wttr.in          # текущее местоположение
-    $ curl wttr.in/svo      # погода в аэропорту Шереметьево (код ICAO: SVO)
-
-Поддерживаемые типы местоположений:
-
-    /paris                  # город
-    /~Eiffel+tower          # любое местоположение
-    /Москва                 # юникодное имя любого местоположения на любом языке
-    /muc                    # код аэропорта ICAO (3 буквы)
-    /@stackoverflow.com     # доменное имя
-    /94107                  # почтовый индекс (только для США)
-    /-78.46,106.79          # GPS-координаты
-
-Специальные условные местоположения:
-
-    /moon                   # Фаза Луны (добавьте ,+US или ,+France для города Moon в США/Франции)
-    /moon@2016-10-25        # Фаза Луны для указанной даты (@2016-10-25)
-
-Единицы измерений:
-
-    ?m                      # метрические (СИ) (используются везде кроме США)
-    ?u                      # USCS (используются в США)
-    ?M                      # показывать скорость ветра в м/с
-
-Опции отображения:
-
-    ?0                      # только текущая погода
-    ?1                      # погода сегодня + 1 день
-    ?2                      # погода сегодня + 2 дня
-    ?n                      # узкая версия (только день и ночь)
-    ?q                      # тихая версия (без текста "Прогноз погоды")
-    ?Q                      # сверхтихая версия (без "Прогноз погоды", нет названия города)
-    ?T                      # отключить терминальные последовательности (без цветов)
-
-PNG-опции:
-
-    /paris.png              # сгенерировать PNG-файл
-    ?p                      # добавить рамочку вокруг
-    ?t                      # transparency=150 (прозрачность 150)
-    transparency=...        # прозрачность от 0 до 255 (255 = не прозрачный)
-
-Опции можно комбинировать:
-
-    /Paris?0pq
-    /Paris?0pq&lang=fr
-    /Paris_0pq.png          # в PNG-запросах опции указываются после _
-    /Rome_0pq_lang=it.png   # длинные опции разделяются знаком подчёркивания _
-
-Локализация:
-
-    $ curl fr.wttr.in/Paris
-    $ curl wttr.in/paris?lang=fr
-    $ curl -H "Accept-Language: fr" wttr.in/paris
-
-Поддерживаемые языки:
-
-    am ar af be bn ca da de el et fr fa gl hi hu ia id it lt mg nb nl oc pl pt-br ro ru ta tr th uk vi zh-cn zh-tw (поддерживаются)
-    az bg bs cy cs eo es eu fi ga hi hr hy is ja jv ka kk ko ky lv mk ml mr nl fy nn pt pt-br sk sl sr sr-lat sv sw te uz zh zu he (в процессе)
-
-Специальные страницы:
-
-    /:help                  # показать эту страницу
-    /:bash.function         # показать рекомендованную функцию wttr()
-    /:translation           # показать список переводчиков wttr.in
-
-"""
-
 import requests
 
 
-city = input('Укажите город или место: \n')
+city_list = [
+    'Лондоне',
+    'Шереметьево',
+    'череповец',
+]
 
-url = f'https://wttr.in/{city}?1nTMqm&lang=ru'
+params = {
+    'n': '',
+    'T': '',
+    'M': '',
+    'q': '',
+    'lang': 'ru',
+}
 
-res = requests.get(url, timeout=5)
 
-print(res.text)
+def get_status_response(res):
+
+    if res.ok:
+        status = 'ok'
+    else:
+        status = 'err'
+        print('Страница не существует\n')
+
+    return status
+
+
+def get_weather():
+
+    for city in city_list:
+
+        url = f'https://wttr.in/{city}'
+
+        res = requests.get(url, params=params, timeout=5)
+
+        status = get_status_response(res)
+
+        if status == 'ok':
+            print(res.text)
+
+
+if __name__ == '__main__':
+    get_weather()
